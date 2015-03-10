a793d98f0b3cee6a0985c7b9eff855621835a3a5
Suresh Reddy Guntaka <sureshreddy.guntaka@outlook.com>
2015-03-11 05:10:33 +0530

    Refactored a little bit

--- exmaple/src/main/java/io/udvi/rpc/example/SpringHelloClient.java
+++ exmaple/src/main/java/io/udvi/rpc/example/SpringHelloClient.java
@@ -5,9 +5,7 @@ import io.udvi.rpc.client.annotation.EnableRpcClient;
 import io.udvi.rpc.example.obj.TestInterface;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 
 /**
